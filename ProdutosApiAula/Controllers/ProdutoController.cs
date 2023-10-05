bea8ecd94118163e71cf46452a94481845c3f242
Alexandre de Oliveira <alexmontanha@hotmail.com>
2023-10-05 11:37:54 -0300

    Adicionar arquivos de projeto.

---
+++ ProdutosApiAula/Controllers/ProdutoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProdutosApiAula.Models;
+using ProdutosApiAula.Repositories;
+
+namespace ProdutosApiAula.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutoController : Controller
+    {
+        [HttpGet(Name = "GetProduto")]
+        public IEnumerable<Produto> Get()
+        {
+            return MockDB.GetProdutos();
+        }
+
+        [HttpGet("{id}")]
+        public Produto GetProduto(int id)
+        {
+            return MockDB.GetProduto(id);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Produto produto)
+        {
+            MockDB.Produtos.Add(produto);
+            return CreatedAtRoute("GetProduto", new { id = produto.Id }, produto);
+        }
+    }
+}
